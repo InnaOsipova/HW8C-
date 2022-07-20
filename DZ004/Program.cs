aa33d0f61bb6188f408fed8e66a39b8457129559
Inna Osipova <rutheva_inna@mail.ru>
2022-07-20 20:59:16 +0300

    Эта задача не работает, оставлю для задать вопрос

---
+++ DZ004/Program.cs
@@ -0,0 +1,80 @@
+﻿//Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого.
+//массив размером 2 x 2 x 2
+
+int [] FillArray (int count)
+{
+    int [] newArray = new int [count];
+    for (int i = 0; i < count; i++)
+    {
+       newArray [i] = new Random().Next(10, 100);
+    }
+    return newArray;
+}
+
+int [] CheckUniqArray (int [] arr)
+{
+    
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        int uniqEllement = arr[i];
+        for (int j = i+1; j < arr.GetLength(0); j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                arr[j] = new Random().Next(10, 100);
+            }
+        }
+    }
+    return arr;
+    
+}
+
+void Printarr (int[,,] arr )
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int h = 0; h < arr.GetLength(2); h++)
+            {
+                Console.Write(arr [i,j,h]+"   ");
+            }
+            System.Console.WriteLine();
+        }
+        System.Console.WriteLine();
+        
+
+       
+    }
+}
+
+int [,,] CreateCube (int []arr, int lines, int columns, int hiaght)
+{
+    int n = 0;
+    int [,,] arrayCube = new int [lines, columns,  hiaght];
+    for (int i = 0; i < lines ; i++){
+    for (int j = 0; j < columns; j++){
+    for (int h = 0; h < hiaght; h++)
+    {
+         arrayCube[i,j,h] = arr[n];
+         n++;
+    }
+    }
+    }
+    
+    return arrayCube;
+}
+
+
+int line = 2;
+int column = 2;
+int hi = 2;
+int numbers = line*column*hi;
+
+int [] matrix = new int [numbers];
+matrix = FillArray(numbers);
+CheckUniqArray(matrix);
+CreateCube(matrix, line, column, hi);
+Printarr(matrix);
+
