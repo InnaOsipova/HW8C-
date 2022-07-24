ada4bccf53404ddf0955f250c37cfdaf1c53438e
Inna Osipova <rutheva_inna@mail.ru>
2022-07-24 12:57:44 +0300

    Заполнение массива спирально 2

---
+++ DZ006/Program.cs
@@ -0,0 +1,33 @@
+﻿//Заполните спирально массив 4 на 4.
+
+int n = 5;
+int x = 0;
+int y = 0;
+int dx = 1;
+int dy = 0;
+int [,] array = new int [n, n];
+
+for (int i = 0; i < n*n; i++)
+{
+    array [y, x] = i+1;
+    int test = 0;
+    if (dx != 0) test = x + dx;
+    else test = y + dy;
+    if (test < 0 || test == n || array[y + dy, x + dx] != 0)
+    {
+        int oldX = dx;
+        dx = -dy;
+        dy = oldX;
+    }
+    y += dy;
+    x += dx;
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write (array [i,j]+"    ");
+    }
+    System.Console.WriteLine();
+}
